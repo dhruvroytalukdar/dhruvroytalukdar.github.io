6b4b83138b50d51930aea58db483f25132f33d96
Dhruv Roy Talukdar <64345884+dhruvroytalukdar@users.noreply.github.com>
2021-12-03 16:11:24 +0530

    classwork change

--- Classwork/prog5/app.js
+++
@@ -1,9 +0,0 @@
-const pic = document.querySelector('#pic')
-
-pic.addEventListener('mouseenter',() => {
-    pic.classList.add('enlarge')
-})
-
-pic.addEventListener('mouseleave',() => {
-    pic.classList.remove('enlarge')
-})
